918d57ed5589a3eb92cdfedb5bf6638494c81be4
Sandro Almeida <sandrodotbuild@gmail.com>
2024-01-17 10:50:11 -0800

    implementing error handling

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -20,26 +20,39 @@ export async function createSnippet(
     formState: { message: string },
     formData: FormData
 ) {
-    const title = formData.get("title");
-    const code = formData.get("code");
+    try {
+        const title = formData.get("title");
+        const code = formData.get("code");
 
-    if (typeof title !== "string" || title.length < 3) {
-        return {
-            message: "Title must be at least 3 characters long",
-        };
+        if (typeof title !== "string" || title.length < 3) {
+            return {
+                message: "Title must be at least 3 characters long",
+            };
+        }
+
+        if (typeof code !== "string" || code.length < 10) {
+            return {
+                message: "Code must be at least 10 characters long",
+            };
+        }
+
+        await db.snippet.create({
+            data: {
+                title,
+                code,
+            },
+        });
+    } catch (err: unknown) {
+        if (err instanceof Error) {
+            return {
+                message: err.message,
+            };
+        } else {
+            return {
+                message: "Unknown error",
+            };
+        }
     }
 
-    if (typeof code !== "string" || title.length < 10) {
-        return {
-            message: "Code must be at least 10 characters long",
-        };
-    }
-
-    const snippet = await db.snippet.create({
-        data: {
-            title,
-            code,
-        },
-    });
     redirect("/");
 }
